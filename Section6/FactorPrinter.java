de45a9054f2f4ee1390bbce46c5828a6a1e4d70c
Ilmo Salomäki <salomaki.ilmo@gmail.com>
2025-02-19 14:10:35 +0200

    5

---
+++ Section6/FactorPrinter.java
@@ -0,0 +1,22 @@
+package Section6;
+
+public class FactorPrinter {
+    
+    public static void main(String[] args) {
+        printFactors(32);
+    }
+
+    public static void printFactors(int number) {
+        if (number < 1) {
+            System.out.println("Invalid Value");
+            return;
+        }
+
+        for (int i = 1; i <= number; i++) {
+            if (number % i == 0) {
+                System.out.println(i);
+            }
+        }
+        System.out.println();
+    }
+}
