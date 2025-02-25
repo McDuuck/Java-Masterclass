038bcb357609df2b0181096da5aab7a422c71b35
Ilmo Salomäki <salomaki.ilmo@gmail.com>
2025-02-25 18:21:25 +0200

    .

---
+++ Section7OopInheritance/Floor/Calculator.java
@@ -0,0 +1,15 @@
+package Section7OopInheritance.Floor;
+
+public class Calculator {
+    private Floor floor;
+    private Carpet carpet;
+
+    public Calculator(Floor floor, Carpet carpet) {
+        this.floor = floor;
+        this.carpet = carpet;
+    }
+
+    public double getTotalCost() {
+        return this.floor.getArea() * this.carpet.getCost();
+    }
+}
