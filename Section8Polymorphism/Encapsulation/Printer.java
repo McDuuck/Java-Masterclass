81ba54a1b2ae9a593000585a2059e6e2109322c0
Ilmo Salomäki <salomaki.ilmo@gmail.com>
2025-02-28 13:24:48 +0200

    .

---
+++ Section8Polymorphism/Encapsulation/Printer.java
@@ -0,0 +1,41 @@
+package Section8Polymorphism.Encapsulation;
+
+public class Printer {
+    
+    private int tonerLevel;
+    private int pagesPrinted;
+    private boolean duplex;
+
+    public Printer(int tonerLevel, boolean duplex) {
+        this.pagesPrinted = 0;
+        this.tonerLevel = (tonerLevel <= 100 && tonerLevel > -1) ? tonerLevel : -1;
+        this.duplex = duplex;
+    }
+
+    public int addToner(int tonerAmount) {
+        if (tonerAmount > 0 && tonerAmount <= 100) {
+            if (this.tonerLevel + tonerAmount > 100) {
+                return -1;
+            } else {
+                this.tonerLevel += tonerAmount;
+                return this.tonerLevel;
+            }
+        } else {
+            return -1;
+        }
+    }
+
+    public int printPages(int pages) {
+        int pagesToPrint = pages;
+        if (this.duplex) {
+            pagesToPrint = (pages / 2) + (pages % 2);
+        }
+        this.pagesPrinted += pagesToPrint;
+        return pagesToPrint;
+    }
+
+    public int getPagesPrinted() {
+        return this.pagesPrinted;
+    }
+
+}
